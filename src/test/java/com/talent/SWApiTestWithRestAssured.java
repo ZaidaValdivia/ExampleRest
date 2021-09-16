6e13cc4f2ac36626fedbed7746ef61137384161b
Zaida Valdivia <zvaldivia11@gmail.com>
2021-09-16 00:23:40 -0400

    fifth commit

--- src/test/java/com/talent/SWApiTestWithRestAssured.java
+++ src/test/java/com/talent/SWApiTestWithRestAssured.java
@@ -1,7 +1,12 @@
 package com.talent;
 
+import com.talent.bind.BaseApiResponse;
 import io.restassured.RestAssured;
+import io.restassured.config.LogConfig;
 import org.testng.annotations.Test;
+
+import static io.restassured.RestAssured.config;
+import static io.restassured.config.LogConfig.logConfig;
 import static org.hamcrest.Matchers.*;
 
 public class SWApiTestWithRestAssured {
@@ -9,92 +14,13 @@ public class SWApiTestWithRestAssured {
     @Test
     public void whenRequestingAResourceThenLinksToResourcesMustBeReturned() {
 
-        BaseApiResponse baseApiResponse = RestAssured
-                .given()
-                .baseUri("https://swapi.dev/api" +
-                        "" +
-                        "" +
-                        "" +
-                        "" +
-                        "" +
-                        "" +
-                        "" +
-                        "" +
-                        "" +
-                        "" +
-                        "" +
-                        "")
-                .and()
-                .queryParam("format", "json")
-                .when()
-                .get("/")
-                .then()
-                .log().all()
-                .and().assertThat().statusCode(is(equalTo(200)))
-                .and()
-                .body("films", response -> notNullValue())
-                .body("vehicles", response -> notNullValue())
-                .body("people", response -> notNullValue())
-                .body("starships", response -> notNullValue())
-                .body("species", response -> notNullValue())
-                .body("planets", response -> notNullValue())
-                .and().extract().body().as(BaseApiResponse.class);
-
         RestAssured
                 .given()
                 .queryParam("format", "json")
-                .log().all()
-                .when()
-                .post(baseApiResponse.getFilms())
-                .then()
-                .log().all()
-                .and()
-                .assertThat()
-                .statusCode(is(equalTo(405)));
+                .config(config().logConfig(logConfig().enableLoggingOfRequestAndResponseIfValidationFails()))
+                .when().get("https://swapi.dev/api/planets/?format=json")
+                .then().assertThat()
+                .statusCode(is(equalTo(200)));
 
-        RestAssured
-                .given()
-                .queryParam("format", "json")
-                .log().all()
-                .when()
-                .post(baseApiResponse.getFilms())
-                .then()
-                .log().all()
-                .and()
-                .assertThat()
-                .statusCode(is(equalTo(405)));
-
-    }
-    private static class BaseApiResponse {
-        private String films;
-        private String vehicles;
-        private String people;
-        private String starships;
-        private String species;
-        private String planets;
-
-        public String getFilms() {
-            return films;
-        }
-
-        public String getVehicles() {
-            return vehicles;
-        }
-
-        public String getPeople() {
-            return people;
-        }
-
-        public String getStarships() {
-            return starships;
-        }
-
-        public String getSpecies() {
-            return species;
-        }
-
-        public String getPlanets() {
-            return planets;
-        }
     }
 }
