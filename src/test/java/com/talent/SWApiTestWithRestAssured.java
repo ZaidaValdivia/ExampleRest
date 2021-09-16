c9c6921c71dfceae97cf1c25100c3d526f25cf33
Zaida Valdivia <zvaldivia11@gmail.com>
2021-09-15 22:52:32 -0400

    third commit

--- src/test/java/com/talent/SWApiTestWithRestAssured.java
+++ src/test/java/com/talent/SWApiTestWithRestAssured.java
@@ -1,7 +1,6 @@
 package com.talent;
 
 import io.restassured.RestAssured;
-import io.restassured.response.ValidatableResponse;
 import org.testng.annotations.Test;
 import static org.hamcrest.Matchers.*;
 
@@ -10,7 +9,7 @@ public class SWApiTestWithRestAssured {
     @Test
     public void requestAresourcesThenLinkToReturn(){
 
-        ValidatableResponse body = RestAssured
+        String body = RestAssured
                 .given()
                 .baseUri("https://swapi.dev/api")
                 .and()
@@ -19,6 +18,15 @@ public class SWApiTestWithRestAssured {
                 .get("/")
                 .then()
                 .log().all()
-                .and().assertThat().statusCode(is(equalTo(200)));
+                .and().assertThat().statusCode(is(equalTo(200)))
+                .and()
+                .body("films", response -> notNullValue())
+                .body("vehicles", response -> notNullValue())
+                .body("people", response -> notNullValue())
+                .body("starships", response -> notNullValue())
+                .body("species", response -> notNullValue())
+                .body("planets", response -> notNullValue())
+                .and().extract().body().asString();
+
     }
 }
