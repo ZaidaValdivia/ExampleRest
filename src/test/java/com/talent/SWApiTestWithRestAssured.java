4cc911dc8dbf5691fe9de1eaf73b879a1b2b46fe
Zaida Valdivia <zvaldivia11@gmail.com>
2021-09-15 22:49:33 -0400

    second commit

--- src/test/java/com/talent/SWApiTestWithRestAssured.java
+++ src/test/java/com/talent/SWApiTestWithRestAssured.java
@@ -1,4 +1,24 @@
 package com.talent;
 
+import io.restassured.RestAssured;
+import io.restassured.response.ValidatableResponse;
+import org.testng.annotations.Test;
+import static org.hamcrest.Matchers.*;
+
 public class SWApiTestWithRestAssured {
+
+    @Test
+    public void requestAresourcesThenLinkToReturn(){
+
+        ValidatableResponse body = RestAssured
+                .given()
+                .baseUri("https://swapi.dev/api")
+                .and()
+                .queryParam("format", "json")
+                .when()
+                .get("/")
+                .then()
+                .log().all()
+                .and().assertThat().statusCode(is(equalTo(200)));
+    }
 }
