f86048442c5834882e2267fa57fe7893d6a8e233
Zaida Valdivia <zvaldivia11@gmail.com>
2021-09-16 01:37:19 -0400

    sexto commit

--- src/test/java/com/talent/SWApiTestWithRestAssured.java
+++ src/test/java/com/talent/SWApiTestWithRestAssured.java
@@ -1,26 +1,86 @@
 package com.talent;
 
-import com.talent.bind.BaseApiResponse;
+
+
+import com.talent.log.LoggerOutputStream;
+import static io.restassured.RestAssured.*;
+
 import io.restassured.RestAssured;
-import io.restassured.config.LogConfig;
+import org.junit.Before;
+import org.slf4j.LoggerFactory;
 import org.testng.annotations.Test;
 
-import static io.restassured.RestAssured.config;
+import org.slf4j.Logger;
+
+
+import java.io.IOException;
+import java.io.PrintStream;
+
 import static io.restassured.config.LogConfig.logConfig;
-import static org.hamcrest.Matchers.*;
+import static io.restassured.config.RestAssuredConfig.config;
+import static org.hamcrest.Matchers.equalTo;
+import static org.hamcrest.Matchers.is;
+
+
+
 
 public class SWApiTestWithRestAssured {
 
-    @Test
-    public void whenRequestingAResourceThenLinksToResourcesMustBeReturned() {
+    //private final Logger logger = LoggerFactory.getLogger(getClass());
 
-        RestAssured
-                .given()
-                .queryParam("format", "json")
-                .config(config().logConfig(logConfig().enableLoggingOfRequestAndResponseIfValidationFails()))
-                .when().get("https://swapi.dev/api/planets/?format=json")
-                .then().assertThat()
-                .statusCode(is(equalTo(200)));
+    @Before
+    public void requestBeforeSwapi(){
+        baseURI = "https://swapi.dev/api/";
+    }
+
+    @Test
+    public void getTestSwapi(){
+        baseURI = "https://swapi.dev/api/";
+
+                given()
+                .log().all()
+                .get("/planets/1/")
+                .then()
+                .log()
+                .ifError()
+                .statusCode(is(equalTo(200)))
+                        .body("population", equalTo("200000"))
+                        .log().all();
 
     }
+
+
+    /*@Test
+    public void whenRequestingAResourceThenLinksToResourcesMustBeReturned() throws IOException {
+
+        try (LoggerOutputStream outputStream = new LoggerOutputStream(logger)) {
+            RestAssured.config = config().logConfig(logConfig()
+                    .defaultStream(new PrintStream(outputStream))
+                    .enableLoggingOfRequestAndResponseIfValidationFails());
+
+            RestAssured
+                    .given()
+                    .log().method().and().log().uri().and().log().params()
+                    .queryParam("format", "json")
+                    .when()
+                    .get("https://swapi.dev/api/")
+                    .then()
+                    .assertThat().statusCode(is(equalTo(200)));
+
+            outputStream.flush();
+
+            RestAssured
+                    .given()
+                    .queryParam("format", "json")
+                    .when()
+                    .post("https://swapi.dev/api/films/")
+                    .then()
+                    .assertThat().statusCode(is(equalTo(200)));
+
+            outputStream.flush();
+        }
+
+    }
+*/
+
 }
